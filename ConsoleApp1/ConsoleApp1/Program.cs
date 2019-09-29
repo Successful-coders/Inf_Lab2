51276a6905ad816705c5a8ac234f6471e733f2a7
Дарья Воронкина <voronkina.2018@azure.nstu.ru>
2019-09-29 09:04:24 +0700

    add file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static List<double> Readfile(string filename)
+        {
+            List<double> numbers = new List<double>();
+            foreach (var line in File.ReadLines(filename))
+            {
+                numbers.Add(double.Parse(line, System.Globalization.CultureInfo.InvariantCulture));
+            }
+            return numbers;
+        }
+        static void Main(string[] args)
+        {
+            List<double> result = new List<double>();
+            result = Readfile("f.txt");
+        }
+    }
+}
