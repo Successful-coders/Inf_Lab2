1335557ac0608cb0d5ddccb455c66be7bee5995b
Дарья Воронкина <voronkina.2018@azure.nstu.ru>
2019-09-29 11:13:48 +0700

    1-5

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,10 +16,55 @@ namespace ConsoleApp1
             }
             return numbers;
         }
+
+        static int getCountsOfDigits(int number)//count of number
+        {
+            int count = (number == 0) ? 1 : 0;
+            while (number != 0)
+            {
+                count++;
+                number /= 10;
+            }
+            return count;
+        }
+        static string FromDec(long n, int p)
+        {
+            var result = "";
+            for (; n > 0; n /= p)
+            {
+                var x = n % p;
+                result = (char)(x < 0 || x > 9 ? x + 'A' - 10 : x + '0') + result;
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            List<double> result = new List<double>();
-            result = Readfile("f.txt");
+            List<double> result = Readfile("f.txt");
+            //List<int> mark = new List<int>();
+            //List<int> exponent = new List<int>();
+            int[] mark = new int[result.Count];
+            int[] exponent = new int[result.Count];
+            string[] result2 = new string[result.Count];
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i] < 0)//0
+                    {
+                        mark[i] = 1;
+                    }
+                else
+                    {
+                        mark[i] = 0;
+                    }
+                result[i] = Math.Abs(result[i]);
+                string str = System.Convert.ToString(result[i]);
+                string[] parts = str.Split(',');
+                exponent[i] = parts[0].Length;//1
+                exponent[i] = Convert.ToInt32(FromDec(exponent[i], 2));//2
+                result[i] = result[i] * Math.Pow(10, parts[1].Length);//3,4
+                result2[i] = FromDec((int)(result[i]), 2);//5
+
+            }
         }
     }
 }
